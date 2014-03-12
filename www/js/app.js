50f396cf03729f8e4577eb858eae8da44bb48eaa
Javier Liarte <jliarte@gmail.com>
2014-03-12 15:59:48 +0100

    Rolling back to localStorage adapter as firefox doesn't support webSQL (http://hacks.mozilla.org/2010/06/comparing-indexeddb-and-webdatabase/)

--- www/js/app.js
+++ www/js/app.js
@@ -2,7 +2,7 @@
 (function () {
 
     /* ---------------------------------- Local Variables ---------------------------------- */
-    var adapter = new WebSqlAdapter();
+    var adapter = new LocalStorageAdapter();
     adapter.initialize().done(function () {
         console.log("Data adapter initialized");
     });
