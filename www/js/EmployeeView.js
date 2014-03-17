09445bc1f48cd211a50876af6ac8728691f5cfe6
Javier Liarte <jliarte@gmail.com>
2014-03-17 13:16:02 +0100

    Adding geolocalization data access to employee view using navigator.geolocation.getCurrentPosition

--- www/js/EmployeeView.js
+++ www/js/EmployeeView.js
@@ -1,6 +1,19 @@
 var EmployeeView = function (adapter, template, employee) {
+	this.addLocation = function (event) {
+		event.preventDefault();
+		navigator.geolocation.getCurrentPosition(
+			function(position) {
+				alert(position.coords.latitude + ',' + position.coords.longitude);
+			},
+			function() {
+				alert('Error getting location');
+			}
+			);
+	}
+	
 	this.initialize = function() {
 		this.el = $('<div/>');
+		this.el.on('click', '.add-location-btn', this.addLocation);
 	};
 
 	this.initialize();
@@ -9,4 +22,6 @@ var EmployeeView = function (adapter, template, employee) {
 		this.el.html(template(employee));
 		return this;
 	}
+
+
 }
